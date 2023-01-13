fd9d964799f8c6b330b3d9eedc79582a83b36378
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-13 11:56:53 +0200

    Add how work resolving

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const {MyMath} = require('./MyMath');
 const {Component} = require('./Component');
-
+//require('test');
 const result = MyMath.mul(2,6);
 console.log('result = ', result);
 
@@ -10,4 +10,15 @@ comp.render();
 
 /*
 resolving -> loading -> wrapping -> evaluation -> caching
+resolving
+1) core modules
+2) file
+    - *.js
+    - *.json
+3) directory 
+    - package.json -> "main" 
+    - index.js
+    - index.json
+4) node_modules  
+5) throw new Error()  
 */
