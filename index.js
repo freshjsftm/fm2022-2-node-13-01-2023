7e0b8a122bd6ffad0fec321e5e25527f24e254a5
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-16 09:43:18 +0200

    Add example fs core module

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-'use strict';
-const {MyMath} = require('./MyMath');
-const {Component} = require('./Component');
-//require('test');
-const result = MyMath.mul(2,6);
-console.log('result = ', result);
-
-const comp = new Component();
-comp.render();
-
+"use strict";
+const fs = require("fs").promises;
+//console.log(fs)
+fs.readFile("./text.txt", { encoding: "utf-8" })
+  .then((content) => {
+    //console.log(content)
+    const newContent = content + "\nnew line\t!\n";
+    fs.writeFile("./assets/newText.txt", newContent)
+        .then()
+        .catch((error) => {
+            console.log('no such file or directory');
+          })
+    //fs.appendFile("./newText.txt", newContent);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+//створити новий файл newText.txt і в цей записати зміст файлу text.txt,
+//а в кінець ще дописати new line!
 /*
 resolving -> loading -> wrapping -> evaluation -> caching
 resolving
