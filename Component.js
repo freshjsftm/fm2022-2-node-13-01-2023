e4700faa76e5fc3107ddab005aa4f9ee32e4e63d
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-16 09:55:24 +0200

    Empty project

--- Component.js
+++
@@ -1,8 +0,0 @@
-const { MyMath } = require("./MyMath");
-class Component {
-  render() {
-    console.log("render");
-  }
-}
-console.log('5+8=',MyMath.sum(5, 8));
-exports.Component = Component;
--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 "use strict";
-const fs = require("fs").promises;
-//console.log(fs)
-fs.readFile("./text.txt", { encoding: "utf-8" })
-  .then((content) => {
-    //console.log(content)
-    const newContent = content + "\nnew line\t!\n";
-    fs.writeFile("./assets/newText.txt", newContent)
-        .then()
-        .catch((error) => {
-            console.log('no such file or directory');
-          })
-    //fs.appendFile("./newText.txt", newContent);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-//створити новий файл newText.txt і в цей записати зміст файлу text.txt,
-//а в кінець ще дописати new line!
+
+
 /*
 resolving -> loading -> wrapping -> evaluation -> caching
 resolving
