486f4219d5b1a1a2a12be59ac1988f09e5ed5f32
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-17 10:42:10 +0200

    Add example named export

--- src/math.js
+++ src/math.js
@@ -1,9 +1,17 @@
-const pow = (base, exp)=> base**exp;
+export const pow = (base, exp)=> base**exp;
 
-const sum = (a, b)=> a+b;
+export const sum = (a, b)=> a+b;
 
-const sub = (a, b)=> a-b;
+export const sub = (a, b)=> a-b;
 
-const div = (a,b)=>a/b;
+export const div = (a,b)=>a/b;
 
-export default {pow, sum, sub, div};
+export const mult = (a,b)=>a*b;
+
+export const aaa = (a,b)=>a*b;
+
+const mathVar = 555;
+
+//export default {pow, sum, sub, div};
+
+export default mathVar;
